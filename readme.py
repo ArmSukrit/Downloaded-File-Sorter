2a884fb882a42d5af4d5302e750b313f7e37600a
Arm Sukrit <arm.xdsk@hotmail.com>
2020-09-27 00:42:02 +0700

    run only one time (no loop), added git ignore, move b64 img and read me to their files

---
+++ readme.py
@@ -0,0 +1,13 @@
+readme = f'- The folder path "sorter path" is scanned once every 5 seconds for new \n' \
+             f'files and sort them into ' \
+             f'their folders based on their extensions\n' \
+             f'- You can change how frequently the program checks and files to be ignored in config.json\n' \
+             f'- To track all moved files, see "FileSorter move log.txt" \n' \
+             f'by pressing Ctrl + C or Delete in the program terminal\n\n' \
+             f'IMPORTANT\n' \
+             f'\tYou should not create new file in this sorter folder, ' \
+             f'because it will be renamed and moved automatically.\n' \
+             f'\tIn other word, sorter path folder is suggested to be used as a download folder.\n\n' \
+             f'\tAnother useful case, you can use this program to sort other folders by removing "config.json"\n' \
+             f'\tlocated in the same folder as the program. After that rerun the program and it will ask you for ' \
+             f'new sorter path.'
